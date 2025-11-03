b4adcadff9cbaec6e2193acb73a4e8cba4dfeca7
kwangsungkim138 <kwangsungkim138@gmail.com>
2025-11-03 17:00:58 +0900

    add korean g2p(phonemizer)

---
+++ matcha/text/korean_g2p.py
@@ -0,0 +1,15 @@
+import re
+from g2pk2 import G2p
+
+_g2p = G2p()
+
+def graphemes_to_phonemes_korean(raw_text: str) -> str:
+    """
+    raw_text: e. g. "한국어로 말하는 법을 배우는 중입니다."
+    return:   "한구거로말하는버블배우는중임니다"
+    """
+    phonemes = _g2p(raw_text)
+    phonemes = pron.strip()
+    phonemes = re.sub(r"\s+", " ", phonemes)
+
+    return phonemes
